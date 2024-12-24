ee868bf5cf13bf445142a0c91f25799390afaafa
Damien Campus <damien.lobato@le-campus-numerique.fr>
2024-12-24 14:54:04 +0100

    stream exercises

--- src/main/java/com/example/demo/StreamExample.java
+++ src/main/java/com/example/demo/StreamExample.java
@@ -7,17 +7,42 @@ import java.util.stream.Collectors;
 public class StreamExample {
     public static void main(String[] args) {
 
+        System.out.println("Exercise 1");
+
         List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 
-        List<Integer> evens = numbers.stream()
+        List<Integer> evenDoubleNumbers = numbers.stream()
                 .filter(number -> number % 2 == 0) // Check if the number is even
-                .collect(Collectors.toList());
+                .map(number -> number * 2) // double each even number
+                .collect(Collectors.toList()); // Collect results in new list
 
-        List<Integer> odds = numbers.stream()
-                .filter(number -> number % 2 != 0) // Check if the number is odd
-                .collect(Collectors.toList());
 
-        System.out.println("Evens: " + evens);
-        System.out.println("Odds: " + odds);
+        System.out.println("Evens numbers doubled " + evenDoubleNumbers + "\n");
+
+        System.out.println("________________________");
+
+        System.out.println("Exercise 2");
+
+        List<String> strings = Arrays.asList("The", "owls", "are not", "what they seem");
+
+        long lengthMoreThanFive = strings.stream()
+                .filter(s -> s.length()>5)
+                .count();
+
+        System.out.println("The number of strings greater than 5 characters are " + lengthMoreThanFive);
+
+        System.out.println("________________________");
+
+        System.out.println("Exercise 3");
+
+        List<String> someStrings = Arrays.asList("Fire", "walk", "with", "me");
+
+        String convertToUpperCaseAndSortAlphabetically = someStrings.stream()
+                .sorted()
+                .map(String::toUpperCase)
+                .collect(Collectors.joining(" "));
+
+        System.out.println(convertToUpperCaseAndSortAlphabetically);
+
     }
 }
